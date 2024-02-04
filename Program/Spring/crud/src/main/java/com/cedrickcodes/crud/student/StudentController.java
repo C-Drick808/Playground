c45174fb4332857b3e290759298443fc171835fb
C-Drick808 <cedrickesd@gmail.com>
2024-02-04 15:25:04 +0800

    Business Layer

--- Program/Spring/crud/src/main/java/com/cedrickcodes/crud/student/StudentController.java
+++ Program/Spring/crud/src/main/java/com/cedrickcodes/crud/student/StudentController.java
@@ -1,7 +1,5 @@
 package com.cedrickcodes.crud.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,19 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
-   @GetMapping
-	public List<Student> getStudents(){
-		return List.of(
-			new Student(
-				1L,
-				"Cedrick John Barrera",
-				"cedbarrera05@gmail.com",
-				LocalDate.of(2001, Month.JULY, 5),
-				22
-			)
-		);
+	private final StudentService studentService;
+
+	public StudentController(StudentService studentService){
+		this.studentService = studentService;
 	}
-   
+
+	@GetMapping
+	public List<Student> getStudents(){
+		return studentService.getStudents();
+	}
+
+
+
+
+
+
 }
 
 
